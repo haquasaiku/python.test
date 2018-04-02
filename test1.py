64b3e2835e39ef3915eee0941cf306afbdcd39f0
haquasaiku <infinityhopecx@gmail.com>
2018-04-02 07:54:00 +0000

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,7 @@
+1 print "Hello World!"
+2 print "Hello Again"
+3 print "I like typing this."
+4 print "This is fun."
+5 print 'Yay! Printing.'
+6 print "I'd much rather you 'not'."
+7 print 'I "said" do not touch this.'
