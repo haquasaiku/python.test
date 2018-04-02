4196560ed0a4759ee9ae9678b09caf89245eff24
haquasaiku <infinityhopecx@gmail.com>
2018-04-02 07:57:53 +0000

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,9 @@
+1 # A comment, this is so you can read your program later.
+2 # Anything after the # is ignored by python.
+3
+4 print "I could have code like this." # and the comment after is ignored
+5
+6 # You can also use a comment to "disable" or comment out a piece of code:
+7 # print "This won't run."
+8
+9 print "This will run."
